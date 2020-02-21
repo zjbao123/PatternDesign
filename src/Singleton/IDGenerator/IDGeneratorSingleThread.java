28d26968407a83b28c7e48665a188f7f03866362
baosk <345232213@qq.com>
2020-02-22 00:46:21 +0800

    rename

--- src/Singleton/IDGenerator/IDGeneratorSingleThread.java
+++ src/Singleton/IDGenerator/IDGeneratorSingleThread.java
@@ -7,18 +7,19 @@ import java.util.concurrent.atomic.AtomicLong;
  * @author zjbao123
  * @version 1.0
  * @since 2020-02-21 23:49:43
+ * 单一线程内单例
  */
-public class IdGeneratorSingleThread {
+public class IDGeneratorSingleThread {
     private AtomicLong id = new AtomicLong(0);
 
-    private static final ConcurrentHashMap<Long, IdGeneratorSingleThread> instances = new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<Long, IDGeneratorSingleThread> instances = new ConcurrentHashMap<>();
 
-    private IdGeneratorSingleThread(){}
+    private IDGeneratorSingleThread(){}
 
-    public static IdGeneratorSingleThread getInstance(){
+    public static IDGeneratorSingleThread getInstance(){
 
         Long currentThreadId = Thread.currentThread().getId();
-        instances.putIfAbsent(currentThreadId,new IdGeneratorSingleThread());
+        instances.putIfAbsent(currentThreadId,new IDGeneratorSingleThread());
         return instances.get(currentThreadId);
     }
 
